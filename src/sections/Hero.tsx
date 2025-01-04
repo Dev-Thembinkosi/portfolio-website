c22781c0a9a4ee6c4ad7d0943beee9eb556d420a
TManhique <manhiquet0@gmail.com>
2025-01-05 00:44:40 +0200

    medium screen hero implementation

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -26,7 +26,7 @@ export const HeroSection = () => {
             performing web applications. Lets discuss your next project.
           </p>
         </div>
-        <div className="flex flex-col items-center mt-8 gap-4">
+        <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
           <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
             <span className="font-semibold">Explore My Work</span>
             <ArrowDown className="size-4" />
