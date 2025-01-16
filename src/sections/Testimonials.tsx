03218692f63d5c239c13404159208bd965dc58e6
TManhique <manhiquet0@gmail.com>
2025-01-16 13:09:12 +0200

    Testimonial mask implementation

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -51,15 +51,29 @@ export const TestimonialsSection = () => {
           description="Do'nt just take my word for it. See what my clients have to say about my work."
         />
 
-        <div>
-          {testimonials.map((testimonial) => (
-           <Card key={testimonial.name}>
-              <Image src={testimonial.avatar} alt={testimonial.name} />
-              <div>{testimonial.name}</div>
-              <div>{testimonial.position}</div>
-              <p>{testimonial.text}</p>
-          </Card>
-          ))}
+        <div className="mt-16 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+          <div className="flex gap-8 flex-none">
+            {testimonials.map((testimonial) => (
+              <Card key={testimonial.name} className="max-w-xs">
+                <div className="flex gap-4 items-center">
+                  <div className="size-14 bg-gray-700 inline-flex items-center justify-center rounded-full">
+                    <Image
+                      src={testimonial.avatar}
+                      alt={testimonial.name}
+                      className="max-h-full"
+                    />
+                  </div>
+                  <div>
+                    <div className="font-semibold">{testimonial.name}</div>
+                    <div className="text-sm text-white/40">
+                      {testimonial.position}
+                    </div>
+                  </div>
+                </div>
+                <p className="mt-4 text-sm">{testimonial.text}</p>
+              </Card>
+            ))}
+          </div>
         </div>
       </div>
     </div>
