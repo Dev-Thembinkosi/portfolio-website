b1290f769adf4cd4534fdfe921301ec2f7f7293b
TManhique <manhiquet0@gmail.com>
2025-01-20 12:55:12 +0200

    My interet: about page updatae

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -3,30 +3,41 @@ import { SectionHeader } from "@/components/SectionHeader";
 import StarIcon from "@/assets/icons/star.svg";
 import Image from "next/image";
 import bookImage from "@/assets/images/book-cover.png";
+import JavaScriptIcon from "@/assets/icons/square-js.svg";
+import HTMLIcon from "@/assets/icons/html5.svg";
+import CssIcon from "@/assets/icons/css3.svg";
+import ReactIcon from "@/assets/icons/react.svg";
+import ChromeIcon from "@/assets/icons/chrome.svg";
+import GithubIcon from "@/assets/icons/github.svg";
+import { TechIcon } from "@/components/TechIcon";
 
-const toolboxItems =[
+const toolboxItems = [
   {
     title: "JavaScript",
-    icon: '',
-
+    iconType: JavaScriptIcon,
   },
+
   {
     title: "JavaScript",
-    icon: '',
-
+    iconType: HTMLIcon,
   },
   {
-    title: "JavaScript",
-    icon: '',
-
+    title: "CSS3",
+    iconType: CssIcon,
   },
   {
-    title: "JavaScript",
-    icon: '',
-
+    title: "React",
+    iconType: ReactIcon,
   },
-]
-
+  {
+    title: "Chrome",
+    iconType: ChromeIcon,
+  },
+  {
+    title: "Github",
+    iconType: GithubIcon,
+  },
+];
 
 export const AboutSection = () => {
   return (
@@ -50,11 +61,30 @@ export const AboutSection = () => {
           <div>
             <StarIcon />
             <h3>My Toolbox</h3>
-            <p>Explore the technologies and tools i use to create my digital experiences.</p>
+            <p>
+              Explore the technologies and tools i use to create my digital
+              experiences.
+            </p>
           </div>
           <div>
-
+            {toolboxItems.map((item) => (
+              <div key={item.title}>
+                <TechIcon component={item.iconType} />
+                <span>{item.title}</span>
+              </div>
+            ))}
           </div>
+        </Card>
+        <Card>
+          <div>
+            <StarIcon />
+            <h3>Beyond the code</h3>
+            <p>Explore my interest and hobbies beyond the code.</p>
+          </div>
+        </Card>
+
+        <Card>
+          
         </Card>
       </div>
     </div>
