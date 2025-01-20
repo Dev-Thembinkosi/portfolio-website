328b0ad82d33805cb487b981091849ee0d2b3f99
TManhique <manhiquet0@gmail.com>
2025-01-20 16:40:47 +0200

    CardHeader component

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -10,6 +10,9 @@ import ReactIcon from "@/assets/icons/react.svg";
 import ChromeIcon from "@/assets/icons/chrome.svg";
 import GithubIcon from "@/assets/icons/github.svg";
 import { TechIcon } from "@/components/TechIcon";
+import mapImage from "@/assets/images/map.png";
+import smileMemoji from "@/assets/images/memoji-smile.png";
+import { CardHeader } from "@/components/CardHeader";
 
 const toolboxItems = [
   {
@@ -39,53 +42,104 @@ const toolboxItems = [
   },
 ];
 
+const hobbies = [
+  {
+    title: "Painting",
+    emoji: "🎨",
+  },
+
+  {
+    title: "Reading",
+    emoji: "📘",
+  },
+
+  {
+    title: "Fitness",
+    emoji: "📖",
+  },
+
+  {
+    title: "Photography",
+    emoji: "📷",
+  },
+
+  {
+    title: "Gaming",
+    emoji: "🎮",
+  },
+
+  {
+    title: "Hiking",
+    emoji: "🥾",
+  },
+
+  {
+    title: "Music",
+    emoji: "🎹",
+  },
+];
+
 export const AboutSection = () => {
   return (
-    <div className="pb-96">
-      <SectionHeader
-        eyebrow="About Me"
-        title="A Glimse into My World"
-        description="Learn more about Who i am, What i do, and What inspires me."
-      />
-      <div>
-        <Card>
-          <div>
-            <StarIcon />
-            <h3>My Reads</h3>
-            <p>Explore the books shaping my perpectives.</p>
-            <Image src={bookImage} alt="Book cover" />
-          </div>
-        </Card>
+    <div className="py-20">
+      <div className="container">
+        <SectionHeader
+          eyebrow="About Me"
+          title="A Glimse into My World"
+          description="Learn more about Who i am, What i do, and What inspires me."
+        />
+        <div className="mt-20">
+          <Card className="h-[320px]">
+            <CardHeader
+              title="My Reads"
+              description="Explore the books shapping my perspective"
+            />
+            <div className="w-40 mx-auto mt-8">
+              <Image src={bookImage} alt="Book cover" />
+            </div>
+          </Card>
 
-        <Card>
-          <div>
-            <StarIcon />
-            <h3>My Toolbox</h3>
-            <p>
-              Explore the technologies and tools i use to create my digital
-              experiences.
-            </p>
-          </div>
-          <div>
-            {toolboxItems.map((item) => (
-              <div key={item.title}>
-                <TechIcon component={item.iconType} />
-                <span>{item.title}</span>
-              </div>
-            ))}
-          </div>
-        </Card>
-        <Card>
-          <div>
-            <StarIcon />
-            <h3>Beyond the code</h3>
-            <p>Explore my interest and hobbies beyond the code.</p>
-          </div>
-        </Card>
+          <Card>
+            <div>
+              <StarIcon />
+              <CardHeader
+                title="My Toolbox"
+                description="Explore the technologies and tools i use to build exceptional digital experiences."
+              />
+            </div>
+            <div>
+              {toolboxItems.map((item) => (
+                <div key={item.title}>
+                  <TechIcon component={item.iconType} />
+                  <span>{item.title}</span>
+                </div>
+              ))}
+            </div>
+          </Card>
+          <Card>
 
-        <Card>
-          
-        </Card>
+
+          <CardHeader
+                title="Beyond the code"
+                description="Explore my interest and hobbies beyond the code."
+              />
+            
+
+            <div>
+              {hobbies.map((hobby) => (
+                <div key={hobby.title}>
+                  <span>{hobby.title}</span>
+                  <span>{hobby.emoji}</span>
+                </div>
+              ))}
+            </div>
+          </Card>
+
+          <Card>
+            <Image src={mapImage} alt="map" />
+            <Image src={smileMemoji} alt="smile memoji" />
+          </Card>
+        </div>
       </div>
     </div>
   );
