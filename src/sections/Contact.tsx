487d4d2b87258616740126e4e34ca92ddb5591cd
TManhique <manhiquet0@gmail.com>
2025-01-24 12:32:44 +0200

    Coctact Section add

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,3 +1,21 @@
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
+
 export const ContactSection = () => {
-  return <div>Contact Section</div>;
+  return (
+    <div className="py-16">
+      <div className="container">
+        <div className="bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center">
+          <h2>Let&apos;s create something amazing together</h2>
+          <p>
+            Ready to bring your next project to life? Let&apos;s connect and
+            discuss how i can help you achieve your goals.
+          </p>
+          <button>
+            <span>Contact Me</span>
+            <ArrowUpRightIcon />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
