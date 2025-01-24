3e96641086fe03d1ab3d6dbd31eb3a5029368014
TManhique <manhiquet0@gmail.com>
2025-01-24 12:50:09 +0200

    Mobile call to action completed

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,18 +1,27 @@
 import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
+import grainImage from "@/assets/images/grain.jpg";
 
 export const ContactSection = () => {
   return (
-    <div className="py-16">
+    <div className="py-16 pt-12">
       <div className="container">
-        <div className="bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center">
-          <h2>Let&apos;s create something amazing together</h2>
-          <p>
+        <div className="bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center relative overflow-hidden z-0">
+          <div
+            className="absolute inset-0 opacity-5 -z-10"
+            style={{
+              backgroundImage: `url(${grainImage.src})`,
+            }}
+          ></div>
+          <h2 className="font-serif text-2xl">
+            Let&apos;s create something amazing together
+          </h2>
+          <p className="text-sm mt-2">
             Ready to bring your next project to life? Let&apos;s connect and
             discuss how i can help you achieve your goals.
           </p>
-          <button>
-            <span>Contact Me</span>
-            <ArrowUpRightIcon />
+          <button className="text-white bg-gray-900 inline-flex items-center px-6 h-12 rounded-xl gap-2 mt-8">
+            <span className="font-semibold">Contact Me</span>
+            <ArrowUpRightIcon className="size-4" />
           </button>
         </div>
       </div>
